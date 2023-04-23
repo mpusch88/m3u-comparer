db4765cd7d319a53d159f9861e4faef550af6e87
Matt Pusch <mpusch88@gmail.com>
2023-04-23 14:13:59 -0700

    .re-added strict mode. added exact match detection and split exact and similar results into two files. added option for rescursive folder search. renamed output.txt config file to settings.txt. cleaned up output file formatting

--- comparer.py
+++ comparer.py
@@ -5,21 +5,30 @@ import sys
 import glob
 import shutil
 import urllib.parse
+from tqdm import tqdm
 from tinytag import TinyTag, TinyTagException
 
 
-def find_matching_files(list1, list2): #, strict_mode):
+def find_matching_and_similar_files(list1, list2, strict_mode):
     matching_files = []
+    similar_files = []
+    added_pairs = set()
 
-    for item1 in list1:
+    for item1 in tqdm(list1, desc='Finding matching and similar files'):
         for item2 in list2:
-            if metadata_matches(item1, item2) and item1["file_name"] != item2["file_name"]:  #, strict_mode)
-                matching_files.append((item1, item2))
+            if metadata_matches(item1, item2, strict_mode):
+                pair_key = frozenset([item1['file_name'], item2['file_name']])
+                if item1["file_name"] == item2["file_name"] and pair_key:
+                    matching_files.append((item1, item2))
+                elif pair_key not in added_pairs:
+                    similar_files.append((item1, item2))
+                    # avoids copying the same file twice - possibly move elsewhere to add pairs to txt file
+                    added_pairs.add(pair_key)
 
-    return matching_files
+    return matching_files, similar_files
 
 
-def metadata_matches(item1, item2): #, strict_mode):
+def metadata_matches(item1, item2, strict_mode):
     match_count = 0
 
     if item1["title"] == item2["title"] and item1["title"]:
@@ -31,19 +40,28 @@ def metadata_matches(item1, item2): #, strict_mode):
     if item1["bitrate"] == item2["bitrate"]:
         match_count += 1
 
-    # if strict_mode == False:
-    return match_count >= 3 and round(item1["length"], 2) == round(item2["length"], 2)
+    if strict_mode == False:
+        return match_count >= 3 and round(item1["length"], 2) == round(item2["length"], 2)
 
-    # else:
-    #     return match_count >= 4 and round(item1["length"], 2) == round(item2["length"], 2)
+    else:
+        return match_count >= 4 and round(item1["length"], 2) == round(item2["length"], 2)
 
 
-def compare_metadata_lists(list1, list2): #, strict_mode):
-    missing_in_list1 = [item for item in list2 if not any(
-        metadata_matches(item, input1) for input1 in list1)] #, strict_mode)
-    missing_in_list2 = [item for item in list1 if not any(
-        metadata_matches(item, input2) for input2 in list2)] #, strict_mode)
+def compare_metadata_lists(list1, list2, strict_mode):
+    missing_in_list1 = []
+    missing_in_list2 = []
 
+    for item in tqdm(list2, desc='Comparing first list'):
+        if not any(metadata_matches(item, input1, strict_mode) for input1 in list1):
+            missing_in_list1.append(item)
+
+    print()
+
+    for item in tqdm(list1, desc='Comparing second list'):
+        if not any(metadata_matches(item, input2, strict_mode) for input2 in list2):
+            missing_in_list2.append(item)
+
+    print()
     return missing_in_list1, missing_in_list2
 
 
@@ -98,10 +116,10 @@ def extract_files_with_metadata(lines):
 
                     files_with_metadata.append(file_data)
 
-                else:
-                    with open(error_file_name, "a", encoding='utf-8') as error_file:
-                        error_file.write(
-                            f"Error processing {file_path}: Invalid audio file\n")
+                # else:
+                #     with open(error_file_name, "a", encoding='utf-8') as error_file:
+                #         error_file.write(
+                #             f"Error processing {file_path}: Invalid audio file\n")
 
             else:
                 with open(error_file_name, "a", encoding='utf-8') as error_file:
@@ -111,17 +129,22 @@ def extract_files_with_metadata(lines):
     return files_with_metadata
 
 
-def scan_folder_for_audio_files(folder_path):
+def scan_folder_for_audio_files(folder_path, recursive):
     audio_files = []
 
     for ext in ('*.mp3', '*.m4a', '*.flac', '*.wav', '*.ogg'):
-        audio_files.extend(
-            glob.glob(os.path.join(folder_path, ext), recursive=True))
+        if recursive:
+            files = glob.glob(os.path.join(
+                folder_path, '**', ext), recursive=True)
+        else:
+            files = glob.glob(os.path.join(folder_path, ext))
+
+        audio_files.extend(files)
 
     return audio_files
 
 
-def process_input(input_path):
+def process_input(input_path, recursive):
     if os.path.isfile(input_path) and (input_path.lower().endswith('.m3u8') or input_path.lower().endswith('.m3u')):
         with open(input_path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
@@ -129,10 +152,12 @@ def process_input(input_path):
         return extract_files_with_metadata(lines)
 
     elif os.path.isdir(input_path):
-        audio_files = scan_folder_for_audio_files(input_path)
+        audio_files = []
+        audio_files = scan_folder_for_audio_files(input_path, recursive)
+
         files_with_metadata = []
 
-        for file_path in audio_files:
+        for file_path in tqdm(audio_files, desc=f'Processing {input_path}'):
             metadata = extract_audio_metadata(file_path)
 
             if metadata:
@@ -148,10 +173,10 @@ def process_input(input_path):
 
                 files_with_metadata.append(file_data)
 
-            else:
-                with open(error_file_name, "a", encoding='utf-8') as error_file:
-                    error_file.write(
-                        f"Error processing {file_path}: Invalid audio file\n")
+            # else:
+            #     with open(error_file_name, "a", encoding='utf-8') as error_file:
+            #         error_file.write(
+            #             f"Error processing {file_path}: Invalid audio file\n")
 
         return files_with_metadata
     else:
@@ -168,9 +193,7 @@ def copy_possible_duplicates(duplicates, folder_path):
         os.makedirs(new_dir, exist_ok=True)
 
         print(
-            f"Copying '{item1['file_path']}' to '{os.path.join(new_dir, item1['file_name'])}'")
-        print(
-            f"Copying '{item2['file_path']}' to '{os.path.join(new_dir, item2['file_name'])}'")
+            f"  Copying possible duplicate pair {index} to \\{new_dir}\\")
 
         shutil.copy2(item1["file_path"], os.path.join(
             new_dir, item1["file_name"]))
@@ -180,37 +203,32 @@ def copy_possible_duplicates(duplicates, folder_path):
 
 if __name__ == "__main__":
 
-    print("\nm3u-comparer v1.0.0\n")
+    print("\nm3u-comparer v1.0.0")
 
     diff_file_name = "diff.txt"
     error_file_name = "errors.txt"
+    similar_file_name = "similar.txt"
     match_file_name = "matches.txt"
     duplicate_folder_name = "duplicates"
 
-    # strict_mode = False
+    strict_mode = False
+    recursive1, recursive2 = False, False
 
-    if os.path.exists("output.txt"):
-        with open("output.txt", "r", encoding='utf-8') as output_files:
+    if os.path.exists("settings.txt"):
+        with open("settings.txt", "r", encoding='utf-8') as output_files:
             for line in output_files:
                 if line.startswith("diff_file_name"):
                     diff_file_name = line.split("=")[1].strip()
                 elif line.startswith("error_file_name"):
                     error_file_name = line.split("=")[1].strip()
+                elif line.startswith("similar_file_name"):
+                    similar_file_name = line.split("=")[1].strip()
                 elif line.startswith("match_file_name"):
                     match_file_name = line.split("=")[1].strip()
                 elif line.startswith("duplicate_folder_name"):
                     duplicate_folder_name = line.split("=")[1].strip()
-                # elif line.startswith("strict_mode"):
-                #     strict_mode = line.split("=")[1].strip().lower() == "true"
-
-    if not os.path.exists(diff_file_name) or not os.path.exists(error_file_name) or not os.path.exists(match_file_name) or not os.path.exists(duplicate_folder_name):
-        print("Error: Invalid output file names")
-        print("Please update or remove output.txt")
-        sys.exit(1)
-
-    clear_file(diff_file_name)
-    clear_file(error_file_name)
-    clear_file(match_file_name)
+                elif line.startswith("strict_mode"):
+                    strict_mode = line.split("=")[1].strip().lower() == "true"
 
     if len(sys.argv) != 3:
         print("Error: Invalid number of arguments")
@@ -221,36 +239,52 @@ if __name__ == "__main__":
 
     input1, input2 = sys.argv[1], sys.argv[2]
 
-    print(
-        f"Comparing inputs:\n\n          {input1} \n\n              and \n\n          {input2}")
+    print(f"    Strict mode: {strict_mode}\n")
 
-    goodLines1_metadata = process_input(input1)
-    goodLines2_metadata = process_input(input2)
+    if os.path.isdir(input1):
+        recursive1 = input(
+            f"Scan {input1} recursively? ('Y' or 'y')   ").lower() == "y"
+
+    if os.path.isdir(input2):
+        recursive2 = input(
+            f"Scan {input2} recursively? ('Y' or 'y')   ").lower() == "y"
+
+    clear_file(diff_file_name)
+    clear_file(error_file_name)
+    clear_file(match_file_name)
+    clear_file(similar_file_name)
+
+    print(
+        f"\n\nComparing inputs:\n\n          {input1} \n\n              and \n\n          {input2}\n")
+
+    print()
+    goodLines1_metadata = process_input(input1, recursive1)
+    print()
+    goodLines2_metadata = process_input(input2, recursive2)
 
     print(f"\n\nExtracted {len(goodLines1_metadata)} files from {input1}")
-    print(f"Extracted {len(goodLines2_metadata)} files from {input2}\n")
+    print(f"Extracted {len(goodLines2_metadata)} files from {input2}\n\n")
 
     missing_in_input2, missing_in_input1 = compare_metadata_lists(
-        goodLines1_metadata, goodLines2_metadata) #, strict_mode)
+        goodLines1_metadata, goodLines2_metadata, strict_mode)
 
-    matching_files = find_matching_files(
-        goodLines1_metadata, goodLines2_metadata) #, strict_mode)
+    matching_files, similar_files = find_matching_and_similar_files(
+        goodLines1_metadata, goodLines2_metadata, strict_mode)
 
     if not missing_in_input1 and not missing_in_input2:
-        print("No differences found!")
-        sys.exit(0)
+        print("\n\nNo differences found!\n")
+    else:
+        print("\n\nDifferences found!\n")
 
-    print("\nDifferences found!\n")
-
-    if os.path.exists(diff_file_name):
         with open(diff_file_name, "w", encoding='utf-8') as diff_file:
-            diff_file.write("Differences found:\n\n")
+            diff_file.write(
+                f"Differences found: {len(missing_in_input1 + missing_in_input2)}\n\n")
 
             if len(missing_in_input1) > 0:
                 diff_file.write(
                     f"    {len(missing_in_input1)} files not in {input1}:\n\n")
                 for metadata in missing_in_input1:
-                    diff_file.write(f"    {metadata['file_name']}\n")
+                    diff_file.write(f"      {metadata['file_name']}\n")
 
             diff_file.write("\n")
 
@@ -258,48 +292,54 @@ if __name__ == "__main__":
                 diff_file.write(
                     f"    {len(missing_in_input2)} files not in {input2}:\n\n")
                 for metadata in missing_in_input2:
-                    diff_file.write(f"    {metadata['file_name']}\n")
+                    diff_file.write(f"      {metadata['file_name']}\n")
 
         print(f"\nWrote results to {diff_file_name}")
 
-    else:
-        print(f"Error: Invalid output path: {diff_file_name}")
-        sys.exit(1)
-
     if matching_files:
         with open(match_file_name, "w", encoding='utf-8') as match_file:
-            match_file.write("Matches with different filenames:\n\n")
+            match_file.write(f"Exact matches: {len(matching_files)}\n\n")
             for item1, item2 in matching_files:
-                match_file.write(
+                match_file.write(f"  {item1['file_name']}  --  {input1}\n")
+                match_file.write(f"  {item2['file_name']}  --  {input2}\n\n")
+
+        print(f"\nWrote exact matches to {match_file_name}")
+
+    if similar_files:
+        with open(similar_file_name, "w", encoding='utf-8') as similar_file:
+            similar_count_line = f"Potential duplicates: {len(similar_files)}\n\n"
+            similar_file.write(similar_count_line)
+            for item1, item2 in similar_files:
+                similar_file.write(
                     f"{item1['file_name']} in {input1} matches {item2['file_name']} in {input2}\n")
 
-                match_file.write(f"    {item1['file_name']} in {input1}:\n")
-                match_file.write(f"        Title: {item1['title']}\n")
-                match_file.write(f"        Artist: {item1['artist']}\n")
-                match_file.write(f"        Album: {item1['album']}\n")
-                match_file.write(
+                similar_file.write(f"    {item1['file_name']} in {input1}:\n")
+                similar_file.write(f"        Title: {item1['title']}\n")
+                similar_file.write(f"        Artist: {item1['artist']}\n")
+                similar_file.write(f"        Album: {item1['album']}\n")
+                similar_file.write(
                     f"        Length: {round(item1['length'], 2)}\n")
-                match_file.write(f"        Bitrate: {item1['bitrate']}\n\n")
+                similar_file.write(f"        Bitrate: {item1['bitrate']}\n\n")
 
-                match_file.write(f"    {item2['file_name']} in {input2}:\n")
-                match_file.write(f"        Title: {item2['title']}\n")
-                match_file.write(f"        Artist: {item2['artist']}\n")
-                match_file.write(f"        Album: {item2['album']}\n")
-                match_file.write(
+                similar_file.write(f"    {item2['file_name']} in {input2}:\n")
+                similar_file.write(f"        Title: {item2['title']}\n")
+                similar_file.write(f"        Artist: {item2['artist']}\n")
+                similar_file.write(f"        Album: {item2['album']}\n")
+                similar_file.write(
                     f"        Length: {round(item2['length'], 2)}\n")
-                match_file.write(f"        Bitrate: {item2['bitrate']}\n\n")
+                similar_file.write(f"        Bitrate: {item2['bitrate']}\n\n")
 
-        print(f"\nWrote possible duplicates to {match_file_name}")
+        print(f"\nWrote possible duplicates to {similar_file_name}")
 
         copy_duplicates = input(
-            "\nCopy duplicates to new folders in the project directory? (y/n): ")
+            "\n\nCopy possible duplicates to new folders in the project directory? (y/n): ")
 
         if copy_duplicates.lower() == "y":
-            print("\nCopying duplicates...")
-            copy_possible_duplicates(matching_files, duplicate_folder_name)
-            print("\nCopied duplicates to new folders in the project directory")
+            print("\nCopying possible duplicates...\n")
+            copy_possible_duplicates(similar_files, duplicate_folder_name)
+            print("\nCopied possible duplicates to new folders in the project directory")
 
     if os.path.exists(error_file_name):
         with open(error_file_name, "r", encoding='utf-8') as error_file:
             if len(error_file.readlines()) > 0:
-                print(f"\nErrors written to {error_file_name}")
+                print(f"\n\nErrors written to {error_file_name}")
